13483b8200946a2380a2b6f824ac9503e503089e
Balaji Jr <babubalaji877841@gmail.com>
2023-07-16 00:12:35 +0530

    commit

--- fetch_data.php
+++ fetch_data.php
@@ -1,5 +1,5 @@
 <?php
-$db_hostmame="127.0.0.1";
+/*$db_hostmame="127.0.0.1";
     $db_username="root";
     $db_password="";
     $db_name="test";
@@ -24,4 +24,30 @@ $db_hostmame="127.0.0.1";
     echo $row['name']."<br/>";
    }
    mysqli_close($conn);
-?>    
+?> */
+
+    $db_hostmame="127.0.0.1";
+    $db_username="root";
+    $db_password="";
+    $db_name="test";
+    $conn = mysqli_connect($db_hostmame,$db_username,$db_password,$db_name);
+    if(!$conn)
+    {
+        echo "Connection failed: ".mysqli_connect_error();
+        exit;
+    }
+    $email=$_POST['email'];
+    $password=$_POST['password'];
+    $sql="select *from users where email='$email' and password='$password'";
+    $result=mysqli_querry($conn,$sql);
+    if(!result)
+    {
+        echo "Error: ".mysqli_error($conn);
+        exit;
+    }
+    if($row)
+    echo "Hello".$row['name']."<br/>";
+    else
+    echo "Login Failed<br/>";
+   mysqli_close($conn);
+?>   
